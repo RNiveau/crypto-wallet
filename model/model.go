ef07745f1c01db91faf79303d8153664bace772e
Romain Niveau <romain.niveau@gmail.com>
2018-09-02 20:13:08 +0200

    Add ParentId in Operation and Currency in Order (WIP)

--- model/model.go
+++ model/model.go
@@ -2,9 +2,11 @@ package model
 
 import (
 	"errors"
+	"log"
 	"time"
 	"gopkg.in/mgo.v2/bson"
 
+	"github.com/rniveau/crypto-wallet/client"
 )
 
 type Currency int
@@ -20,6 +22,8 @@ type Operation struct {
 	SellOrder   *Order        `json:"sell_order"`
 	Currency    *Currency     `json:"currency"`
 	Description string        `json:"description"`
+	ParentId	string		  `json:"parent_id" bson:",omitempty"`
+	Parent 		*Operation 	  `json:"parent" bson:"-"`
 }
 
 func (operation *Operation) Valid() error {
@@ -35,18 +39,35 @@ func (operation *Operation) Valid() error {
 	if *operation.Currency < Bitcoin || *operation.Currency >= End {
 		return errors.New("Currency is not valid")
 	}
-	if *operation.Currency != Euro && operation.BuyOrder.EuroPrice == 0 {
-		return errors.New("EuroPrice can't be empty in crypto currency order")
-	}
-	return nil
+	if operation.ParentId != "" {
+           parent := client.GetOperation(operation.ParentId)
+           log.Println(parent)
+           if parent == nil {
+           	return errors.New("ParentId doesn't exist")
+           }
+    }
+    return nil
 }
 
 type Order struct {
 	TotalPrice float64   `json:"total_price"`
 	UnitPrice  float64   `json:"unit_price"`
 	EuroPrice  float64   `json:"euro_price"`
+	Currency   *Currency `json:"currency"`
 	Date       time.Time `json:"date"`
 }
+
+func (order *Order) Valid() error {
+	if order.Currency == nil {
+		return errors.New("Currency can't be nil")
+	}
+	if *order.Currency < Bitcoin || *order.Currency >= End {
+		return errors.New("Currency is not valid")
+	}	
+	return nil
+}
+
+
 
 type Budget struct {
 	Id       bson.ObjectId `json:"id" bson:"_id,omitempty"`
